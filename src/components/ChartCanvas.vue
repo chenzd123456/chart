7b521676817ed39dbe338a1af6875c01aa2119da
Adam <chenzd123456@gmail.com>
2024-08-07 14:27:34 +0800

    create project

---
+++ src/components/ChartCanvas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <canvas ref="chart_canvas_ref"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUpdated, ref } from 'vue'
+import Chart from 'chart.js/auto'
+
+const props = defineProps(['config'])
+
+const chart_canvas_ref = ref()
+
+let chart
+
+onMounted(() => {
+  chart = new Chart(chart_canvas_ref.value, props.config)
+})
+
+onUpdated(() => {
+  chart.resize()
+})
+</script>
